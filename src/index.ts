6ab693049904058343e3b20bc77a692083f63a1d
Ion Villarreal <ivillarreal@agetic.gob.bo>
2024-05-22 16:00:00 -0400

    refactor: reestructuración de estructura de proyecto

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express from 'express';
-import newsRoutes from './newsRoutes';
-import { logger } from './logger';
+import newsRoutes from './news/newsRoutes';
+import authRoutes from './authorization/authRoutes';
+import {logger} from './logger';
 import cors from 'cors';
+import userRoutes from "./users/userRoutes";
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(express.json());
 app.use(logger);
 app.use('/news', newsRoutes);
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
