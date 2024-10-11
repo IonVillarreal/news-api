e1b9623a124a9ed204f5b9d27253a0e393d8461f
Ion Villarreal <ivillarreal@agetic.gob.bo>
2024-10-11 09:06:44 -0400

    feat: adición de funciones de revalidación usando api de NextJS

--- src/news/newsController.ts
+++ src/news/newsController.ts
@@ -1,6 +1,17 @@
 import {Request, Response} from 'express';
 import * as newsService from './newsService';
 import 'dotenv/config'
+import axios from "../config/axios";
+
+async function revalidatePage(path: string) {
+    try {
+        const revalidateUrl = `${process.env.NEXT_JS_URL}/api/revalidate?secret=${process.env.REVALIDATION_TOKEN}&path=${path}`;
+        await axios.post(revalidateUrl);
+        console.log(`Revalidated path: ${path}`);
+    } catch (error) {
+        console.error('Error revalidating:', error);
+    }
+}
 
 export const getAllNews = async (req: Request, res: Response): Promise<void> => {
     const page = parseInt(req.query.page as string) || 1;
@@ -22,6 +33,9 @@ export const getNewsById = async (req: Request, res: Response): Promise<void> =>
 export const createNews = async (req: Request, res: Response): Promise<void> => {
     const newsData = req.body;
     const news = await newsService.createNews(newsData);
+
+    await revalidatePage('/news');
+
     res.status(201).json(news);
 };
 
@@ -30,6 +44,10 @@ export const updateNews = async (req: Request, res: Response): Promise<void> => 
     const newsData = req.body;
     try {
         const news = await newsService.updateNews(id, newsData);
+
+        await revalidatePage('/news');
+        await revalidatePage(`/news/${id}`);
+
         res.json(news);
     } catch (error) {
         res.status(404).json({message: 'News not found'});
@@ -40,6 +58,10 @@ export const deleteNews = async (req: Request, res: Response): Promise<void> => 
     const id = parseInt(req.params.id);
     try {
         const news = await newsService.deleteNews(id);
+
+        await revalidatePage('/news');
+        await revalidatePage(`/news/${id}`);
+
         res.json(news);
     } catch (error) {
         res.status(404).json({message: 'News not found'});
